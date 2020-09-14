5d46a2faa062c4564130bb6eff5496b1b6342ff0
Zed Tan <zed@shootbird.work>
2020-09-14 17:31:50 +0200

    removed line that downloads data on individual cases (not used for current analysis)

--- zz-get-data.sh
+++ zz-get-data.sh
@@ -1,8 +1,8 @@
 DATENOW=$(date +%Y-%m-%d)
 YEST=$(date -v-1d +%Y-%m-%d) # macos
 FILEONE="_data/COVID-19_aantallen_gemeente_cumulatief-$DATENOW.csv"
-FILETWO="_data/COVID-19_casus_landelijk$DATENOW.csv"
+# FILETWO="_data/COVID-19_casus_landelijk$DATENOW.csv"
 if $(! test -f "$FILEONE"); then curl https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.csv -o $FILEONE; fi
-if $(! test -f "$FILETWO"); then curl https://data.rivm.nl/covid-19/COVID-19_casus_landelijk.csv -o $FILETWO; fi
+# if $(! test -f "$FILETWO"); then curl https://data.rivm.nl/covid-19/COVID-19_casus_landelijk.csv -o $FILETWO; fi
 echo replacing $YEST with $DATENOW && sed -i .bak "13,54s/$YEST/$DATENOW/g" nl-covid-eda.ipynb
 python3 -m jupyter nbconvert --to notebook --inplace --execute nl-covid-eda.ipynb # run notebook
